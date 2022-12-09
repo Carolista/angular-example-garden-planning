bdd0126b5d7cf12c4bee5741fdd99592be06b38f
Carolista <cjones.renaissancewoman@gmail.com>
2022-12-08 22:54:06 -0600

    added main component with fetched plants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Plant } from '../shared/plant';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  plants: Plant[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loadPlants();
+  }
+
+  async loadPlants() {
+    let response = await fetch('https://docs.google.com/document/d/1DjGYPISRGCsP4zHwJYBtciqbKAvae1CZ9EKsci4mTJk/export?format=txt');
+    let data = await response.json();
+    // console.log(data);
+    for (let i=0; i < data.length; i++) {
+      this.plants.push(
+        new Plant(
+          data[i].id,
+          data[i].name,
+          data[i].color,
+          data[i].numAvailable,
+          data[i].image
+        )
+      )
+    }
+    // console.log(this.plants);
+  }
+
+}
